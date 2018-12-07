4cf325d5934640e2dd54554e5f0a40dda981d93b
kamenev.pa <kamenev.pa@skbkontur.ru>
2018-12-07 15:15:08 +0700

    initial_commit

---
+++ trpo/trpo/IPersistent.cs
@@ -0,0 +1,14 @@
+﻿namespace trpo
+{
+    public interface IPersistent<T>
+    {
+        void Undo();
+        void Redo();
+
+        T this[int index] { get; set; }
+
+        void Add(T elem, int index);
+
+        T Remove(int index);
+    }
+}
---
+++ trpo/trpo/PersistentArray.cs
@@ -0,0 +1,31 @@
+﻿namespace trpo
+{
+    public class PersistentArray<T>: IPersistent<T>
+    {
+        public void Undo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Redo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public void Add(T elem, int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Remove(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
